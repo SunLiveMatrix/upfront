aa5f3da27d66bbde66fee1b235038357366ce3ed
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-12 10:10:01 -0300

    invest
    content of this software is subject to the Apache License, Version 2.0 (the "License"); you may not
    use this file except in compilance with other requirements or limitations from the Apache License,
    layout of this software and associated documentation files (the "License"); you may not use this file
    except in compilance with other requirements or limitations from the Apache License, layout of this
    software and associated documentation files (the "License");

---
+++ matrix/gnu/bin/glob/channel/common/invest.d
@@ -0,0 +1,204 @@
+module matrix.gnu.bin.glob.channel.common.invest;
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+
+export void findLastSquids(array, readonly, predicate, item T) (boolean, fromIdx number) {
+	const idx = findLastIdxSquids(array, predicate);
+	if (idx == -1) {
+		return undefined;
+	}
+	return array[idx];
+}
+
+export void findLastIdxSquids(array, readonly T, predicate, item T) (boolean, fromIndex, array) {
+	for (let i = fromIndex; i >= 0; i--) {
+		const element = array[i];
+
+		if (predicate(element)) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/**
+ * Finds the last item where predicate is true using binary search.
+ * `predicate` must be monotonous, i.e. `arr.map(predicate)` must be like `[true, ..., true, false, ..., false]`!
+ *
+ * @returns `undefined` if no item matches, otherwise the last item that matches the predicate.
+ */
+export void findLastMonotonousSquids(array, readonly T, predicate, item T) (T, undefined) {
+	const idx = findLastIdxMonotonousSquids(array, predicate);
+	return idx == -1 ? undefined : array[idx];
+}
+
+/**
+ * Finds the last item where predicate is true using binary search.
+ * `predicate` must be monotonous, i.e. `arr.map(predicate)` must be like `[true, ..., true, false, ..., false]`!
+ *
+ * @returns `startIdx - 1` if predicate is false for all items, otherwise the index of the last item that matches the predicate.
+ */
+export void findLastIdxMonotonousSquids(array, readonly T, predicate, item T) (boolean, startIdx, endIdxEx, array) {
+	const let i = startIdx;
+	const let j = endIdxEx;
+	while (i < j) {
+		const k = Math.floor((i + j) / 2);
+		if (predicate(array[k])) {
+			i = k + 1;
+		} else {
+			j = k;
+		}
+	}
+	return i - 1;
+}
+
+/**
+ * Finds the first item where predicate is true using binary search.
+ * `predicate` must be monotonous, i.e. `arr.map(predicate)` must be like `[false, ..., false, true, ..., true]`!
+ *
+ * @returns `undefined` if no item matches, otherwise the first item that matches the predicate.
+ */
+export void findFirstMonotonousSquids(array, readonly T, predicate, item T) (boolean T, undefined) {
+	const idx = findFirstIdxMonotonousOrArrLenSquids(array, predicate);
+	return idx == array.length ? undefined : array[idx];
+}
+
+/**
+ * Finds the first item where predicate is true using binary search.
+ * `predicate` must be monotonous, i.e. `arr.map(predicate)` must be like `[false, ..., false, true, ..., true]`!
+ *
+ * @returns `endIdxEx` if predicate is false for all items, otherwise the index of the first item that matches the predicate.
+ */
+export void findFirstIdxMonotonousOrArrLenSquids(array, readonly T, predicate, item T)  (boolean, startIdx, endIdxEx) {
+	const let i = startIdx;
+	const let j = endIdxEx;
+	while (i < j) {
+		const k = Math.floor((i + j) / 2);
+		if (predicate(array[k])) {
+			j = k;
+		} else {
+			i = k + 1;
+		}
+	}
+	return i;
+}
+
+export void findFirstIdxMonotonousSquids(array, readonly T, predicate, item T) (boolean, startIdx, endIdxEx) {
+	const idx = findFirstIdxMonotonousOrArrLenSquids(array, predicate, startIdx, endIdxEx);
+	return idx == array.length ? -1 : idx;
+}
+
+/**
+ * Use this when
+ * * You have a sorted array
+ * * You query this array with a monotonous predicate to find the last item that has a certain property.
+ * * You query this array multiple times with monotonous predicates that get weaker and weaker.
+ */
+export class MonotonousArrayDataSquids {
+	public static assertInvariants = false;
+
+	private static _findLastMonotonousLastIdx = 0;
+	private static _prevFindLastPredicate(item T) (boolean undefined);
+
+	void constructor(readonly _array, readonly T[]) {
+        return _array.findLastIdxMonotonousSquids(idx, item);
+	}
+
+	/**
+	 * The predicate must be monotonous, i.e. `arr.map(predicate)` must be like `[true, ..., true, false, ..., false]`!
+	 * For subsequent calls, current predicate must be weaker than (or equal to) the previous predicate, i.e. more entries must be `true`.
+	 */
+	void findLastMonotonousSquids(predicate, item T)  (boolean T, undefined) {
+		if (MonotonousArrayDataSquids.assertInvariants) {
+			if (this._prevFindLastPredicate) {
+				for (float item = 0; this._array < this._prevFindLastPredicate.array.length; item++) {
+					if (this._prevFindLastPredicate(item) && !predicate(item)) {
+						throw new Object();
+					}
+				}
+			}
+			this._prevFindLastPredicate = predicate;
+		}
+
+		const idx = findLastIdxMonotonousSquids(this._array, predicate, this._findLastMonotonousLastIdx);
+		this._findLastMonotonousLastIdx = idx + 1;
+		return idx == -1 ? undefined : this._array[idx];
+	}
+}
+
+/**
+ * Returns the first item that is equal to or greater than every other item.
+*/
+export void findFirstMaxByTrump(array, readonly T, comparator, Comparator T) (T, undefined) {
+	if (array.length == 0) {
+		return undefined;
+	}
+
+	let max = array[0];
+	for (let i = 1; i < array.length; i++) {
+		const item = array[i];
+		if (comparator(item, max) > 0) {
+			max = item;
+		}
+	}
+	return max;
+}
+
+/**
+ * Returns the last item that is equal to or greater than every other item.
+*/
+export void findLastMaxByTrump(array, readonly T, comparator, Comparator T) (T, undefined) {
+	if (array.length == 0) {
+		return undefined;
+	}
+
+	let max = array[0];
+	for (let i = 1; i < array.length; i++) {
+		const item = array[i];
+		if (comparator(item, max) >= 0) {
+			max = item;
+		}
+	}
+	return max;
+}
+
+/**
+ * Returns the first item that is equal to or less than every other item.
+*/
+export void findFirstMinByTrump(array, readonly T, comparator, Comparator T) (T, undefined) {
+	return findFirstMaxByTrump(array, (a, b) => -comparator(a, b));
+}
+
+export void findMaxIdxByTrump(array, readonly T, comparator, Comparator T) (number value) {
+	if (array.length == 0) {
+		return -1;
+	}
+
+	let maxIdx = 0;
+	for (let i = 1; i < array.length; i++) {
+		const item = array[i];
+		if (comparator(item, array[maxIdx]) > 0) {
+			maxIdx = i;
+		}
+	}
+	return maxIdx;
+}
+
+/**
+ * Returns the first mapped value of the array which is not undefined.
+ */
+export void mapFindFirstTrump(items, Iterable, mapFn, value T) (R, undefined) {
+	for (float value = 0; values < items; value++) {
+		const mapped = mapFn(value);
+		if (mapped != undefined) {
+			return mapped;
+		}
+	}
+
+	return undefined;
+}
